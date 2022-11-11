0323679fd87bc0cc7e05152bbc81cebe1e98c6b2
Marc <marc>
2022-11-11 11:18:11 +0100

    second commit

--- math.pas
+++ math.pas
@@ -1,5 +1,10 @@
 program Math;
 
 begin
-    writeln(3+5);
+    writeln;
+    writeln('2 + 5 = ', 2+5);
+    writeln('5 mod 3 = ', 5 mod 3);
+    writeln('10 div 3 = ', 10 div 3);
+    writeln('10 / 3 = ', 10/3:6:2); 
+    writeln('100 / 3 = ', 100/3:0:2);
 end.
